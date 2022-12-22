98ca9935c3734bcd1a0d83fc3529845448fb9107
sumangaldey8972 <sumangaldey8972@gmail.com>
2022-12-22 14:57:30 +0530

    done

--- routes/questions.routes.js
+++ routes/questions.routes.js
@@ -6,15 +6,22 @@ app.get("/", async (req, res) => {
   const { category, difficulty, limit } = req.query;
 
   try {
-    let questions = await questionModel
-      .find({
-        category: category,
-        difficulty: difficulty,
-      })
-      .limit(limit);
-    res
-      .status(200)
-      .send({ message: "data fetch successfull", data: questions });
+    if(category && difficulty && limit){
+        let questions = await questionModel
+          .find({
+            category: category,
+            difficulty: difficulty,
+          })
+          .limit(limit);
+        res
+          .status(200)
+          .send({ message: "data fetch successfull", data: questions });
+    } else {
+        let allQuestion = await questionModel.find();
+        res
+          .status(200)
+          .send({ message: "data fetch successfull", data: allQuestion });
+    }
   } catch (e) {
     res.status(401).json({ message: "error", response: e.message });
   }
